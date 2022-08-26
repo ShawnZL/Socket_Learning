4b2292b39161902d07aa9bcde5c74f9ec0de2b41
Shawn Zhao <shawnzhao@ShawndeMacBook-Pro.local>
2022-08-26 19:56:52 +0800

    UDP_learning

--- temp/main.c
+++ temp/main.c
@@ -2,20 +2,79 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <ctype.h>
 #include <arpa/inet.h>
 #include <sys/socket.h>
 #define BUF_SIZE 1024
+#define OPSZ 4
 void error_handling(char *message);
+int calculate(int opnum, int opnds[], char operator);
+int main(int argc, char* argv[]) {
+    int serv_sock, clnt_sock;
+    int recv_len, recv_cnt;
+    char opinfo[BUF_SIZE];
+    int result, opnd_cnt, i;
+    struct sockaddr_in serv_addr, clnt_addr;
+    socklen_t clnt_addr_size;
 
-int main() {
-    char message[1023] = "1234";
-    char *stop;
-    printf("%d\n", strtol(message,&stop,10));
-    printf("%s", stop);
+    if (argc != 2) {
+        printf("Usage: %s <port>\n", argv[0]);
+        exit(1);
+    }
+
+    serv_sock = socket(AF_INET, SOCK_STREAM, 0);
+
+    if (serv_sock == -1)
+        error_handling("socket() error");
+
+    memset(&serv_addr, 0, sizeof(serv_addr));
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_addr.s_addr = inet_addr(INADDR_ANY);
+    serv_addr.sin_port = htons(atoi(argv[1]));
+
+    if (bind(serv_sock, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) == -1)
+        error_handling("bind() error");
+
+    if (listen(serv_sock, 5) == -1)
+        error_handling("listen() error");
+
+    clnt_addr_size = sizeof(clnt_addr);
+    for (i = 0; i < 5; ++i) {
+        opnd_cnt = 0;
+        clnt_sock = accept(serv_sock, (struct sockaddr*) &clnt_addr, &clnt_addr_size);
+        // 返回文件描述符
+        read(clnt_sock, &opnd_cnt, 1);
+        recv_len = 0;
+        while ((opnd_cnt*OPSZ+1) > recv_len) {
+            recv_cnt = read(clnt_sock, &opinfo[recv_len], BUF_SIZE - 1);
+            recv_len += recv_cnt;
+        }
+        result = calculate(opnd_cnt, (int*)opinfo, opinfo[recv_len - 1]);
+        write(clnt_sock, (char*)&result, sizeof(result));
+        close(clnt_sock);
+    }
+    close(serv_sock);
+    return 0;
 }
 
 void error_handling(char *message) {
     fputs(message, stderr);
     fputc('\n', stderr);
     exit(1);
+}
+
+int calculate(int opnum, int opnds[], char operator) {
+    int result = opnds[0], i;
+    switch(operator) {
+        case '+':
+            for (i = 1; i < opnum; ++i) result += opnds[i];
+            break;
+        case '-':
+            for (i = 1; i < opnum; ++i) result -= opnds[i];
+            break;
+        case '*':
+            for (i = 1; i < opnum; ++i) result *= opnds[i];
+            break;
+    }
+    return result;
 }
